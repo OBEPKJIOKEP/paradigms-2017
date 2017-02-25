23e55e934ed3c909fa9d5853a3abdb6d1aaa7d93
OBEPKJIOKEP <gpevnev@niuitmo.ru>
2017-02-25 20:28:58 +0300

    Added contract for ArrayQueueModule

--- Homework/3/ArrayQueueModule.java
+++ Homework/3/ArrayQueueModule.java
@@ -1,10 +1,22 @@
 /**
  * Created by Greg on 23.02.2017.
  */
+
+// inv: begin ≥ 0 ∧ end ≥ 0 ∧ size ≥ 0 ∧ (end > begin, ∀i ∈ [begin, end) a[i] ≠ null ∨ 
+// ∨ (end <= begin ∧ size ≠ 0), ∀i ∈ [begin, a.size] ∪ [0, end) a[i] ≠ null)
+
+// immutable = begin = begin' ∧ end = end' ∧ 
+// ((end > begin, ∀i ∈ [begin, end) a[i] = a[i]') ∨ 
+// ∨ (end <= begin ∧ size ≠ 0, ∀i ∈ [begin, a.size] ∪ [0, end) a[i] = a[i]'))
 public class ArrayQueueModule {
     private static int begin = 0, end = 0, size = 0;
     private static Object[] elements = new Object[5];
 
+    // pre: element ≠ null
+    // post: end = (end' + 1) % a.length ∧ size = size' + 1 ∧
+    // ∧ begin = begin' ∧ a[end'] = element ∧ 
+    // ∧ ((end' > begin, ∀i ∈ [begin, end') a[i] = a[i]') ∨ 
+    // ∨ (end' ≤ begin ∧ size ≠ 0, ∀i ∈ [begin, a.size] ∪ [0, end') a[i] = a[i]'))
     public static void enqueue(Object element) {
         assert element != null;
 
@@ -14,12 +26,19 @@ public class ArrayQueueModule {
         size++;
     }
 
+    // pre: size > 0
+    // post: R = a[begin] ∧ immutable
     public static Object element() {
         assert size > 0;
 
         return elements[begin];
     }
 
+    // pre: size > 0
+    // post: R = a[begin] ∧ begin = (begin' + 1) % a.length ∧ 
+    // ∧ size = size' - 1 ∧ end = end' ∧
+    // ∧ ((end > begin, ∀i ∈ [begin, end) a[i] = a[i]') ∨ 
+    // ∨ (end ≤ begin ∧ size ≠ 0, ∀i ∈ [begin, a.size] ∪ [0, end) a[i] = a[i]')) 
     public static Object dequeue() {
         assert size() > 0;
 
@@ -29,14 +48,18 @@ public class ArrayQueueModule {
         size--;
         return result;
     }
-
+    
+    // post: R = size ∧ immutable
     public static int size() {
         return size;
     }
 
+    // post: R = (size == 0) ∧ immutable
     public static boolean isEmpty() {
         return size == 0;
     }
+
+    // post: begin = 0 ∧ end = 0 ∧ size = 0  
     public static void clear() {
         begin = 0;
         end = 0;
